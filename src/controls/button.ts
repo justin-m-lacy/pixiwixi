35857bd0b611afe4ba7efac45313bb5acd169579
Justin Lacy <justin@launchbadge.com>
2022-11-28 15:45:26 -0800

    all button to define child in constructor

--- src/controls/button.ts
+++ src/controls/button.ts
@@ -1,17 +1,28 @@
 import { Pane, PaneOptions } from '../panes/pane';
+import { DisplayObject, Container } from 'pixi.js';
 export class Button extends Pane {
 
 	/**
 	 * 
 	 * @param {DisplayObject} clip 
 	 */
-	constructor(opts?: PaneOptions & { onClick?: () => void }) {
+	constructor(opts?: PaneOptions & { onClick?: () => void, child?: Container }) {
 
 		super(opts);
 
 		this.interactive = true;
-		if (opts?.onClick) {
-			this.on('pointerup', opts.onClick);
+		if (opts) {
+			if (opts.onClick) {
+				this.on('pointerup', opts.onClick);
+			}
+			if (opts.child) {
+				const child = this.addChild(opts.child);
+
+				child.position.set(
+					(this.width - child.width) / 2,
+					(this.height - child.height) / 2,
+				);
+			}
 		}
 
 	}
